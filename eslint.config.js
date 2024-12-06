bcf87cd1b2333bac37e969ce40144752718fd179
Jad <joubran.jad@gmail.com>
2024-12-06 08:24:45 +0200

    Add react compiler + eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 export default [
   { ignores: ["dist"] },
@@ -22,6 +23,7 @@ export default [
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "react-compiler": reactCompiler,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -34,6 +36,7 @@ export default [
         { allowConstantExport: true },
       ],
       "react/prop-types": "off",
+      "react-compiler/react-compiler": "error",
     },
   },
 ];
