43f659533f691747fa90f7050f96d123046e6752
Jad Joubran <joubran.jad@gmail.com>
2025-01-14 11:21:01 +0100

    Reorganise code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
-import { ErrorBoundary } from "react-error-boundary";
-
 import Navbar from "./Navbar";
 import Landing from "./Landing";
 import Login from "./Login";
@@ -10,56 +8,45 @@ import Products from "./Products";
 import ProductDetails from "./ProductDetails";
 import Cart from "./Cart";
 
-function Fallback({ error }) {
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
-    </div>
-  );
-}
-
 function App() {
   const [user, setUser] = useState(null);
 
   return (
-    <ErrorBoundary FallbackComponent={Fallback}>
-      <BrowserRouter>
-        <div className="wrapper-gray">
-          <div className="container">
-            <Navbar user={user} />
-          </div>
+    <BrowserRouter>
+      <div className="wrapper-gray">
+        <div className="container">
+          <Navbar user={user} />
         </div>
-        <div className="container page-wrapper">
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/login" element={<Login setUser={setUser} />} />
-            <Route
-              path="/profile"
-              element={<Profile user={user} setUser={setUser} />}
-            />
-            <Route
-              path="/products"
-              element={
-                <Suspense fallback={<p className="loading">Loading...</p>}>
-                  <Products />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/products/:id"
-              element={
-                <Suspense fallback={<p className="loading">Loading...</p>}>
-                  <ProductDetails />
-                </Suspense>
-              }
-            />
-            <Route path="/cart" element={<Cart user={user} />} />
-            <Route path="*" element={<h1>Page not found</h1>} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </ErrorBoundary>
+      </div>
+      <div className="container page-wrapper">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login setUser={setUser} />} />
+          <Route
+            path="/profile"
+            element={<Profile user={user} setUser={setUser} />}
+          />
+          <Route
+            path="/products"
+            element={
+              <Suspense fallback={<p className="loading">Loading...</p>}>
+                <Products />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/products/:id"
+            element={
+              <Suspense fallback={<p className="loading">Loading...</p>}>
+                <ProductDetails />
+              </Suspense>
+            }
+          />
+          <Route path="/cart" element={<Cart user={user} />} />
+          <Route path="*" element={<h1>Page not found</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CartProvider } from "./CartContext";
+import { ErrorBoundary } from "react-error-boundary";
 import App from "./App.jsx";
 import "./index.css";
 
 const queryClient = new QueryClient();
 
+function Fallback({ error }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+    </div>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </QueryClientProvider>
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <QueryClientProvider client={queryClient}>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </QueryClientProvider>
+    </ErrorBoundary>
   </StrictMode>
 );
