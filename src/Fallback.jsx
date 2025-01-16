4e8ec4d104637e7457a3f66451d0894acd94a389
Jad Joubran <joubran.jad@gmail.com>
2025-01-16 13:37:24 +0100

    Refactor to AppSetup

--- src/Fallback.jsx
+++
@@ -1,8 +0,0 @@
-export default function Fallback({ error }) {
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
-    </div>
-  );
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,35 @@
+/* eslint-disable react-refresh/only-export-components */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CartProvider } from "./CartContext";
 import { ErrorBoundary } from "react-error-boundary";
-import Fallback from "./Fallback.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
+function Fallback({ error }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+    </div>
+  );
+}
+
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ErrorBoundary FallbackComponent={Fallback}>
-      <QueryClientProvider client={queryClient}>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </QueryClientProvider>
-    </ErrorBoundary>
-  </StrictMode>
-);
+function AppSetup() {
+  return (
+    <StrictMode>
+      <ErrorBoundary FallbackComponent={Fallback}>
+        <QueryClientProvider client={queryClient}>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </QueryClientProvider>
+      </ErrorBoundary>
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<AppSetup />);
