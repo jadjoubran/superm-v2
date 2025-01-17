bd1265f5225fcbe7fa19acfc09f48b90c132cc4f
Jad <joubran.jad@gmail.com>
2025-01-17 09:16:19 +0100

    Refactor fetcher

--- src/fetcher.jsx
+++ src/fetcher.jsx
@@ -1,12 +1,12 @@
 const BASE_URL = "https://bnwdxdujgzrzgtfkysmy.supabase.co/rest/v1/";
 
-const apitoken =
+const apikey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJud2R4ZHVqZ3pyemd0Zmt5c215Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYyMjUxMDIsImV4cCI6MjA0MTgwMTEwMn0.D0nuB2PYrkIVuIsz3R2JqJLJYHmr8gXChAiZrTGMiHk";
 
 export function get(endpoint) {
   return fetch(BASE_URL + endpoint, {
     headers: {
-      apikey: apitoken,
+      apikey,
     },
   }).then((response) => response.json());
 }
@@ -15,7 +15,7 @@ export function callApi(method, endpoint, data) {
   return fetch(BASE_URL + endpoint, {
     method: method,
     headers: {
-      apikey: apitoken,
+      apikey,
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
