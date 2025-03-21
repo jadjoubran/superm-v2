8951d55c355196329627dbded074082d16e13f27
Jad Joubran <joubran.jad@gmail.com>
2025-03-21 09:30:54 +0100

    Add title to Landing component

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router";
 export default function Landing() {
   return (
     <>
+      <title>SuperM</title>
       <h1>Online shopping simplified</h1>
       <p className="tagline text-dimmed">
         Order your groceries from SuperM with our easy to use app, and get your
