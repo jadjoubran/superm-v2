95a184478f805700462872d8ca09f4fcafd4839e
Jad Joubran <joubran.jad@gmail.com>
2025-03-26 10:35:53 +0100

    Remove unnecessary fragment

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -13,22 +13,20 @@ export default function Profile({ user, onUserLogout }) {
   }
 
   return (
-    <>
-      <div className="profile-wrapper">
-        <h1>Profile</h1>
-        <title>Profile | SuperM</title>
-        <p className="text-dimmed">
-          You are logged in as <strong>{user.username}</strong>.
-        </p>
-        <div className="profile-buttons">
-          <input
-            type="button"
-            value="Logout"
-            className="btn"
-            onClick={handleLogout}
-          />
-        </div>
+    <div className="profile-wrapper">
+      <h1>Profile</h1>
+      <title>Profile | SuperM</title>
+      <p className="text-dimmed">
+        You are logged in as <strong>{user.username}</strong>.
+      </p>
+      <div className="profile-buttons">
+        <input
+          type="button"
+          value="Logout"
+          className="btn"
+          onClick={handleLogout}
+        />
       </div>
-    </>
+    </div>
   );
 }
